9f02087a62d6863884c3ae5d901682e079d7a233
Vhatever <126778223+Vhatever@users.noreply.github.com>
2023-05-20 12:56:46 +0300

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,45 @@
+﻿variable "aws_access_key" {
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "eu-north-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "subnet_cidr_block" {
+  description = "CIDR block for subnet"
+  type        = string
+}
+
+variable "aws_availability_zone" {
+  description = "AWS availability zone"
+  type        = string
+}
+
+variable "ec2_ami" {
+  description = "AMI for EC2 instances"
+  type        = string
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ec2_key_name" {
+  description = "EC2 key pair name"
+  type        = string
+  default     = "your-key-pair"
+}
+
